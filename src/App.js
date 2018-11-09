85d4a995fb62391b980b4f607488dd3ccb3adb93
Julia Heidinger <juliaheidinger@gmx.net>
2018-11-09 12:51:51 +0100

    Add toggleDone function

--- src/App.js
+++ src/App.js
@@ -4,16 +4,36 @@ import Todo from './Todo'
 
 class App extends Component {
   state = {
-    isDone: false
+    gifts: [
+      { text: 'Gutschein', done: true },
+      { text: 'Kaktus', done: false },
+      { text: 'Blumen', done: true },
+      { text: 'Kochtopf', done: false }
+    ]
   }
 
-  gifts = ['Gutschein', 'Kamera-Stativ', 'Tee', 'Kaktus']
+  toggleDone = index => {
+    const { gifts } = this.state
+    const newGifts = [
+      ...gifts.slice(0, index),
+      { ...gifts[index], done: !gifts[index].done },
+      ...gifts.slice(index + 1)
+    ]
+
+    this.setState({
+      gifts: newGifts
+    })
+  }
 
   render() {
     return (
       <ul>
-        {this.gifts.map(gift => (
-          <Todo text={gift} />
+        {this.state.gifts.map((gift, index) => (
+          <Todo
+            text={gift.text}
+            done={gift.done}
+            onClick={() => this.toggleDone(index)}
+          />
         ))}
       </ul>
     )
--- src/Todo.js
+++ src/Todo.js
@@ -3,9 +3,9 @@ import './Todo.css'
 
 export default class Todo extends Component {
   render() {
-    const { onClick, text, isDone } = this.props
+    const { text, done, onClick } = this.props
     return (
-      <li onClick={onClick} className={isDone ? 'Todo done' : 'Todo'}>
+      <li onClick={onClick} className={done ? 'done' : undefined}>
         {text}
       </li>
     )
